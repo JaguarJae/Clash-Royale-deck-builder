9af9d5f66e969dc40dada67920ca7986f75f370e
JaguarJAE <Jaiarjest@gmail.com>
2026-05-06 19:25:44 +0200

    optimized code requests

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import cr_api
 
 #user_tag = input("Clash Royale ID: ")
-my_user_tag = "#QULPLURYG"
-player_tag = my_user_tag
-min_level = 15
+my_tag = "#QULPLURYG"
+player_tag = my_tag
+min_level = 14
 max_level_diff = 16 - min_level
-
-raw_top = cr_api.get_top()
 
 player_cards = cr_api.get_user_cards(player_tag)
 
@@ -17,10 +15,8 @@ def check_deck_level(deck):
         user_card_level = int(player_cards[card_name]["level"])
         level_diff = card_max_level - user_card_level
         if level_diff <= max_level_diff:
-            #print(card["name"], "level: valid")
             continue
         else:
-            #print(card["name"], "level: non-valid")
             return False
     return True
 
--- cr_api.py
+++ cr_api.py
@@ -2,9 +2,9 @@ import requests
 from dotenv import load_dotenv
 import os
 from urllib.parse import quote
+from concurrent.futures import ThreadPoolExecutor
 
 load_dotenv()
-
 token = os.getenv("TOKEN")
 
 base_url = "https://api.clashroyale.com/v1"
@@ -12,14 +12,17 @@ headers = {
     "Authorization" : f"Bearer {token}"
 }
 
+session = requests.Session()
+session.headers = headers
+
 def get_user_info(raw_user_tag):
     user_tag = quote(raw_user_tag)
     url = f"{base_url}/players/{user_tag}"
-    response = requests.get(url, headers=headers)
+    response = session.get(url, headers=headers)
     return response.json()
 
 def get_top():
-    url = f"{base_url}/locations/global/pathoflegend/players?limit=50"
+    url = f"{base_url}/locations/global/pathoflegend/players"
     response = requests.get(url=url, headers=headers)
     return response.json()["items"]
 
@@ -30,10 +33,12 @@ def get_user_cards(user_tag):
 
 def get_top_decks():
     top = get_top()
-    top_decks = []
-    for player in top:
+    def fetch_deck(player):
         player_info = get_user_info(player["tag"])
-        player_deck = player_info["currentDeck"]
-        top_decks.append(player_deck)
+        return player_info["currentDeck"]
+    
+    with ThreadPoolExecutor(max_workers=15) as executor:
+        top_decks = list(executor.map(fetch_deck, top))
+    
     return top_decks
 
